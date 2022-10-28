8e11762bd94274b6ad606df0e13a28e818d8b97e
Vozudo <jploperatobon@gmail.com>
2022-10-28 03:01:21 -0500

    [Task 6] Console 1

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contains the class HBNBCommand"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """This class that contains the entry point of the command interpreter"""
+    prompt: "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command"""
+        return True
+
+    def do_EOF(self, line):
+        """Closes the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """doesn't execute anything"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
