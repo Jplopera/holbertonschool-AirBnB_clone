77175a121f2ada0b2372de1f1187155fdf78481a
Vozudo <jploperatobon@gmail.com>
2022-10-28 11:27:16 -0500

    [Task 5] json

--- models/base_model.py
+++ models/base_model.py
@@ -38,13 +38,13 @@ class BaseModel:
         """updates the public instance attribute updated_at with the current
         datetime"""
         self.updated_at = datetime.datetime.now()
-        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the
         instance"""
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
         return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,19 +19,22 @@ class FileStorage:
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        with open(self.__file_path, mode='w', encoding='utf-8') as file:
-            data = {key: v.to_dict() for key, v in self.__objects.items()}
-            json.dump(data, file)
+        dict1 = {}
+        for key, value in FileStorage.__objects.items():
+            dict1.update([(key, value.to_dict())])
+
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(dict1))
 
     def reload(self):
         """Deserializes the JSON file to __objects (only if the JSON file"""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, mode='r', encoding='utf-8') as file:
-                json_dict = json.loads(file.read())
-                for key, v in json_dict.items():
-                    self.__objects[key] = eval(v['__class__'])(**v)
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                str1 = f.read()
+
+                fromjson = json.loads(str1)
+                FileStorage.__objects = {key: eval(f"{val['__class__']})(**{val})") for key, val in fromjson.items()}
