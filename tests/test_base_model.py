b3475c94fb44065a7137e2c221092622264d9f03
Vozudo <jploperatobon@gmail.com>
2022-10-28 00:07:38 -0500

    [Task 3] Test save method

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""Unittest for class BaseModel"""
+
+
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
+
+class TestBase(unittest.TestCase):
+    """Test Cases for the BaseModel class."""
+
+    def test_save(self):
+        """Test methot save"""
+        b_1 = BaseModel()
+        b_1.save()
+        self.assertFalse(b_1.created_at == b_1.updated_at)
