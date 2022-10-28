9f704c3dc7ffba4bb2998a8f7e9f3fe0a7459004
Vozudo <jploperatobon@gmail.com>
2022-10-28 00:56:38 -0500

    [Task 4] Update to __init__

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,22 @@ class BaseModel:
     """class BaseModel that defines all common attributes/methods
     for other classes"""
 
-    def __init__(self):
-        """Initialize BaseModel0"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel"""
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.updated_at = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """__str__ method that returns a string"""
