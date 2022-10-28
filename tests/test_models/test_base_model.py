966308fd1c46e37380ec6dd44dab84b529fe1d0d
Vozudo <jploperatobon@gmail.com>
2022-10-28 00:32:44 -0500

    [Task 3] Test to_dic method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,5 +16,13 @@ class TestBase(unittest.TestCase):
         b1.save()
         self.assertFalse(b1.created_at == b1.updated_at)
 
+    def test_dic(self):
+        """Test method to_dic"""
+
+        b1 = BaseModel()
+        b1.name = "Juan"
+        b2 = b1.to_dict()
+        self.assertEqual(b2["name"], b1.name)
+
 if __name__ == '__main__':
     unittest.main()
